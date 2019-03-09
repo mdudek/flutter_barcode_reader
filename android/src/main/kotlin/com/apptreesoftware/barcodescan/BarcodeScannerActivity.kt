45ee4c941f534b8dc69354e47405463ad57ecd20
bbedward <bbedward@gmail.com>
2019-03-09 14:49:19 -0500

    Migrate the rest

--- android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
+++ android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
@@ -6,8 +6,8 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
